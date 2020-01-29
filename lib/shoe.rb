9c1bd17c758388fd7062249c4840e43df7c325f1
Alehandro Persaud <andrew041994@gmail.com>
2020-01-29 02:14:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,16 @@
 class Shoe
+  attr_accessor :brand :color :size :material :condition
   def initialize(brand)
     @brand = brand
   end
 
-  def brand=(brand)
-    @brand = brand
-  end
-
-  def brand
-    @brand
-  end
+  # def brand=(brand)
+  #   @brand = brand
+  # end
+  # 
+  # def brand
+  #   @brand
+  # end
 
 
 
