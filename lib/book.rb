5b98cad039db9026d86c3c426e4088e430a4f424
Alehandro Persaud <andrew041994@gmail.com>
2020-01-29 02:18:07 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,32 +7,32 @@ class Book
   # def title=(title)
   #   @title = title
   # end
-  # 
+  #
   # def title
   #   @title
   # end
-  # 
+  #
   # def author=(author)
   # @author = author
   # end
-  # 
+  #
   # def author
   #@author
-  # 
+  #
   # end
-  # 
+  #
   # def page_count=(page_count)
   #   @page_count = page_count
   # end
-  # 
+  #
   # def page_count
   #   @page_count
   # end
-  # 
+  #
   # def genre=(genre)
   #   @genre = genre
   # end
-  # 
+  #
   # def genre
   #   @genre
   # end
