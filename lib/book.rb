e02fd8b41a1c8def208f63404f76bb4cfd7e7ece
Alehandro Persaud <andrew041994@gmail.com>
2020-01-29 02:07:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -36,12 +36,13 @@ class Book
     @genre
   end
 
-  def turn_page=(turn_page)
-    @turn_page = turn_page
-  end
+  # def turn_page=(turn_page)
+  #   @turn_page = turn_page
+  # end
 
   def turn_page
-    @turn_page
+    # @turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 
 
